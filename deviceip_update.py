83d12afd30cfd73a59abd91e6f461fff837da0c0
Andreas Henriksson <andreaskhenriksson@gmail.com>
2018-12-09 10:13:28 +0100

    Added `pick` command that lets you pick device from a list

--- deviceip_update.py
+++ deviceip_update.py
@@ -13,17 +13,17 @@ import sys
 from pathlib import Path
 import os
 
-VAR_FILE = '~/Dropbox/bin/theos.sh'
+VAR_FILE = "~/Dropbox/bin/theos.sh"
 
 def get_config():
     directory = os.path.dirname(os.path.abspath(__file__))
-    with open(directory + '/config.json') as data_file:    
+    with open(directory + "/config.json") as data_file:    
         return json.load(data_file)
 
 
 def get_ssid():
-    objc.loadBundle('CoreWLAN',
-                    bundle_path='/System/Library/Frameworks/CoreWLAN.framework',
+    objc.loadBundle("CoreWLAN",
+                    bundle_path="/System/Library/Frameworks/CoreWLAN.framework",
                     module_globals=globals())
 
     interfaceNames = CWInterface.interfaceNames()
@@ -39,26 +39,36 @@ def get_ssid():
 
 
 def update_with_device(device):
-    print("Picked device: " + device['name'] + ", IP: " + device['IP'] + ", port:", device['port'])
+    print("Picked device: " + device["name"] + ", IP: " + device["IP"] + ", port:", device["port"])
 
-    file_path = VAR_FILE.replace('~', str(Path.home()))
+    file_path = VAR_FILE.replace("~", str(Path.home()))
     with open(file_path, "w") as file:
-        file.write("export THEOS_DEVICE_IP=" + device['IP'] + '\n')
-        file.write("export THEOS_DEVICE_PORT=" + str(device['port']) + '\n')
+        file.write("export THEOS_DEVICE_IP=" + device["IP"] + "\n")
+        file.write("export THEOS_DEVICE_PORT=" + str(device["port"]) + "\n")
+
+
+def pick_from_network(network):
+    devices = network["devices"]
+    for i, device in enumerate(devices):
+        print("{}: {} ({})".format(i + 1, device["name"], device["IP"]))
+    number = int(input("Pick a device: "))
+    update_with_device(devices[number - 1])
 
 
 def update_with_network(network):
     device_name = None
     if (len(sys.argv) > 1):
+        if sys.argv[1] == "pick":
+            return pick_from_network(network)
         device_name = sys.argv[1]
 
-    print("picked network: " + network["name"])
+    print("Picked network: " + network["name"])
     default_device = None
-    for device in network['devices']:
-        if (device_name != None and device_name == device['name']):
+    for device in network["devices"]:
+        if (device_name != None and device_name == device["name"]):
             return update_with_device(device)
 
-        if (device['name'] == network['default']):
+        if (device["name"] == network["default"]):
             if (device_name == None):
                 return update_with_device(device)
             default_device = device
@@ -68,21 +78,21 @@ def update_with_network(network):
 
 def scan_networks():
     default_network = None
-    for network in config['networks']:
-        if ('IP' in network and ip.startswith(network['IP'].split('*')[0])):
-            print("starts with: " + network['IP'])
+    for network in config["networks"]:
+        if ("IP" in network and ip.startswith(network["IP"].split("*")[0])):
+            print("IP starts with: " + network["IP"])
             update_with_network(network)
             return
-        elif (ssid and ssid.startswith(network['name'].split('*')[0])):
-            print("found name: " + network['name'])
+        elif (ssid and ssid.startswith(network["name"].split("*")[0])):
+            print("Found SSID: " + network["name"])
             update_with_network(network)
             return
 
-        if (network['name'] == config['default']):
-            print("found default network")
+        if (network["name"] == config["default"]):
+            print("Found default SSID")
             default_network = network
 
-    print("did not find anything, setting default")
+    print("Did not find anything, setting default")
     update_with_network(default_network)
 
 
